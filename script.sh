4ec8bd7a13e20b3ae561e98897db79b002db84a2
Sai Chaparala <cskrishna9306@gmail.com>
2025-02-17 15:00:03 -0800

    Uppdated script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,11 @@
 mvn clean compile
-javac -cp lib/antlr-4.13.1-complete.jar:target/main -d target/main src/main/XPathProcessor.java
+javac -cp lib/antlr-4.13.1-complete.jar:target/main -d target/main src/main/Main.java
 #java -cp lib/antlr-4.13.1-complete.jar:target/main XPathProcessor "doc(\"j_caesar.xml\")//SCENE"
+java -cp lib/antlr-4.13.1-complete.jar:target/main Main src/main/j\_caesar.xml q1.txt result.xml
 
 # Find CAESAR eq speaker
 #java -cp lib/antlr-4.13.1-complete.jar:target/main XPathProcessor "doc(\"j_caesar.xml\")//SPEECH[.//SPEAKER = .//SPEAKER[text() = \"CAESAR\"]]"
-java -cp lib/antlr-4.13.1-complete.jar:target/main XPathProcessor "doc(\"j_caesar.xml\")//SPEECH[.//SPEAKER eq .//SPEAKER[text() = \"CAESAR\"]]"
+#java -cp lib/antlr-4.13.1-complete.jar:target/main XPathProcessor "doc(\"j_caesar.xml\")//SPEECH[.//SPEAKER eq .//SPEAKER[text() = \"CAESAR\"]]"
 
 # SPEAKER not eq CAESAR
 #java -cp lib/antlr-4.13.1-complete.jar:target/main XPathProcessor "doc(\"j_caesar.xml\")//SPEECH[not .//SPEAKER = .//SPEAKER[text() = \"CAESAR\"]]"
